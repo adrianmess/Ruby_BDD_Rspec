1f594fe63bdc75eabf4ca93864d978bd44303685
Adrian Messado <adrian.messado@gmail.com>
2018-12-08 12:34:38 -0800

    add pry debug tool and test

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,5 +1,9 @@
+require('pry')
+
 def title_case(title)
     split_sentence = title.split
+    # binding.pry adds a breakpoint to our code
+binding.pry
     split_sentence.each do |word|
         word.capitalize!()
     end
